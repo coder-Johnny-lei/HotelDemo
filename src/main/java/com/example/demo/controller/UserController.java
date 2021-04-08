76e11e68542909170dfb7342e77a57e813f0d444
coder-Johnny-lei <73863454+coder-Johnny-lei@users.noreply.github.com>
2021-04-08 17:36:53 +0800

    Controller Standard

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -26,34 +26,6 @@ public class UserController {
     @Autowired
     private TokenService tokenService;
 
-    @PostMapping("/doLogin")
-    public Map<String,Object> doLogin(@RequestBody User user) {
-        Map<String,Object> map=new HashMap<String,Object>();
-        String userName = user.getUserName();
-        String password = user.getPassword();
-        User user2 = userService.findUserByUserName(userName);
-        if (user2 == null) {
-            map.put("code",1);
-            map.put("msg","用户名错误,请重新输入");
-            return map;
-        } else if (!user2.getPassword().equals(password)) {
-            map.put("code",1);
-            map.put("msg","密码输入错误");
-            return  map;
-        } else {
-            //登录成功
-            String token = TokenUtil.sign(userName,password);
-            System.out.println(token);
-            //登录成功
-            map.put("code",0);
-            map.put("token",token);
-            map.put("userId",user2.getUserId());
-            map.put("msg","登录成功");
-            return map;
-        }
-    }
-
-
     @PostMapping("/login")
     public Map<String,Object> login(@RequestBody User user, HttpServletResponse response){
         Map<String,Object> map=new HashMap<String,Object>();
@@ -82,12 +54,6 @@ public class UserController {
 
     }
 
-    @UserLoginToken
-    @GetMapping("/getMessage")
-    public String getMessage(){
-        System.out.println(TokenUtil.getTokenUserId());
-        return "您已通过验证";
-    }
 
     @UserLoginToken
     @GetMapping("/list")
